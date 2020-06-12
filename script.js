0b8d5442e57ef6be94b5cba0507fed4705b8f1b3
igorvac <igorvac@gmail.com>
2020-06-11 22:34:10 -0300

    V1 alpha

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-function granilite(quart) {
-  let pos = [0, 0];
+function granilite(x, y, siz) {
+  let pos = [x, y];
+  const orign = pos[0];
   let poly = [];
   let colors = ["#BC7B46", "#D9B576", "#638692"];
   noFill();
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
-      console.log(`${pos[0]} x ${pos[1]}`);
+      //console.log(`${pos[0]} x ${pos[1]}`);
       push();
       translate(pos[0], pos[1]);
-      stroke(30);
-      //rect(0, 0, quart, quart);
-      stroke(255);
-      let d = [random(0, quart), random(0, quart)];
-      point(d[0], d[1]);
+      //stroke(30);
+      //rect(0, 0, siz, siz);
+      //stroke(255);
+      let d = [random(0, siz), random(0, siz)];
+      //point(d[0], d[1]);
       poly.push(d[0] + pos[0]);
       poly.push(d[1] + pos[1]);
-      pos[0] += quart;
+      pos[0] += siz;
       pop();
     }
-    pos[0] = 0;
-    pos[1] += quart;
+    pos[0] = orign;
+    pos[1] += siz;
   }
   pos = [0, 0];
   fill(colors[parseInt(random(0, 3))]);
-  //strokeWeight(4);
+  strokeWeight(4);
   //stroke(255,0,0);
   //point(poly[2], poly[3]);
   //point(poly[4], poly[5]);
@@ -55,6 +56,12 @@ function setup() {
 }
 
 function draw() {
-  granilite(30);
+  let posX = [0];
+  let posY = [0];
+
+  for (let i = 0; i < 200; i++) {
+    granilite(random(0, width), random(0, height), random(2, 6));
+  }
+
   noLoop();
 }
